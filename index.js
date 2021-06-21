835dab8291684a1935e095b56d903773c9577429
mildtotoro <pimrampha.m@gmail.com>
2021-06-21 19:46:55 +0700

    add file index.js

---
+++ index.js
@@ -0,0 +1,65 @@
+const https = require("https");
+
+const [fundName = ""] = process.argv.slice(2);
+console.log("Fund Name: ", fundName);
+
+const options = {
+  protocol: "https:",
+  hostname: "codequiz.azurewebsites.net",
+  // port: <<HOST PORT>>,
+  method: "GET", // RESQUEST METHOD
+  //   port: 8080,
+  headers: {
+    // 'Content-Type': 'application/json',
+    // 'Content-Length': Buffer.byteLength(postData)
+    Cookie: "hasCookie = true",
+  },
+};
+
+const req = https.request(options, (response) => {
+  let data = "";
+
+  //   console.log(response);
+  response.on("data", (chunk) => {
+    // data response
+    data += chunk;
+    // console.log("chunk", chunk);
+  });
+
+  response.on("end", () => {
+    const start = data.search("<table>");
+    const end = data.search("</table>");
+    const htmlText = data.slice(start, end);
+    const temp1 = htmlText
+      .replace("<table>", "")
+      .replace(/<td>/g, "")
+      .replace(/<tr>/g, "")
+      .replace(/<th>/g, "")
+      .replace(/ /g, "");
+    const temp2 = temp1.split("</tr>");
+
+    const fundList = temp2
+      .filter((item) => item !== "")
+      .map((row, i) => {
+        if (i === 0) {
+          const [fundName, nav, bid, offer, change] = row.split("</th>");
+          return { fundName, nav, bid, offer, change };
+        } else {
+          const [fundName, nav, bid, offer, change] = row.split("</td>");
+          return { fundName, nav, bid, offer, change };
+        }
+      });
+
+    const fund = fundList.find((fund) => {
+      return fund.fundName === fundName;
+    });
+    if (fund) {
+      console.log(fund.nav);
+    }
+  });
+});
+
+req.on("error", (e) => {
+  console.log("Problem with request:", e.message);
+});
+req.end();
